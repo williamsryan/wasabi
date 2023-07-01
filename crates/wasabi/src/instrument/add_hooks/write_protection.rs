6afdede4396671caf4758779167eec2d50d40b2a
Mike Lappas <lappas.m@northeastern.edu>
2023-07-01 13:50:00 -0400

    [Write Protection] Do not protect functions that are called by the 'start' function

--- crates/wasabi/src/instrument/add_hooks/write_protection.rs
+++ crates/wasabi/src/instrument/add_hooks/write_protection.rs
@@ -17,25 +17,53 @@ struct ValidateWritePatch {
     func_idx: Idx<Function>,
 }
 
+fn get_called_func_idxs_recursive(
+    module: &Module,
+    func_idx: &Idx<Function>,
+    func_idxs: &mut Vec<Idx<Function>>,
+) {
+    func_idxs.push(*func_idx);
+
+    if let Some(code) = module.function(*func_idx).code() {
+        for instr in &code.body {
+            if let Call(func_idx) = instr {
+                get_called_func_idxs_recursive(module, func_idx, func_idxs);
+            }
+        }
+    } else {
+        println!(
+            "[Write Protection] Failed to parse function #{0} !",
+            func_idx.to_usize()
+        );
+    }
+}
+
 pub fn write_protect_range(module: &mut Module, start_address: u32, end_address: u32) {
     assert!(
         start_address < end_address,
         "start_address ({start_address:#010X}) < end_address ({end_address:#010X})"
     );
 
-    // println!("start_address ({start_address:#010X}) < end_address ({end_address:#010X})");
+    let mut whitelisted_func_idxs = vec![];
+    // If the module contains a 'start' function
+    if let Some(start_func_idx) = module.start {
+        // Take note of all the functions that the 'start' function calls
+        get_called_func_idxs_recursive(module, &start_func_idx, &mut whitelisted_func_idxs);
+        // Ensure that no duplicate values exist in the vector
+        whitelisted_func_idxs.sort();
+        whitelisted_func_idxs.dedup();
+    }
 
     let mut validate_write_patches: Vec<ValidateWritePatch> = vec![];
     let mut patched_store_instrs = 0;
     for (func_idx, func) in module.clone().functions() {
-        if let Some(start_func_idx) = module.start {
-            if func_idx == start_func_idx {
-                continue;
-            }
+        // Do not protect any functions that are called by the 'start' function
+        if whitelisted_func_idxs.contains(&func_idx) {
+            continue;
         }
 
-        if let Some(func_code) = func.code() {
-            for (instr_idx, instr) in func_code.body.clone().into_iter().enumerate() {
+        if let Some(code) = func.code() {
+            for (instr_idx, instr) in code.body.clone().into_iter().enumerate() {
                 if let Store(store_op, _) = instr {
                     let func_type = store_op.to_type();
                     let store_addr_type = func_type.inputs()[0];
@@ -128,14 +156,19 @@ pub fn write_protect_range(module: &mut Module, start_address: u32, end_address:
                         }
                     };
 
-                    if let Some(module_code) = module.functions[func_idx.to_usize()].code_mut() {
-                        module_code.body[instr_idx] = Call(validate_write_func_idx);
+                    if let Some(code) = module.functions[func_idx.to_usize()].code_mut() {
+                        code.body[instr_idx] = Call(validate_write_func_idx);
                         patched_store_instrs += 1;
                     } else {
                         println!("[Write Protection] Failed to patch instruction #{0} in function #{1} !", instr_idx + 1, func_idx.to_usize());
                     }
                 }
             }
+        } else {
+            println!(
+                "[Write Protection] Failed to parse function #{0} !",
+                func_idx.to_usize()
+            );
         }
     }
     println!(
