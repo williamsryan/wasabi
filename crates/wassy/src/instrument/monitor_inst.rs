9db8e2e887d1483882cfc592ccaa741b4a713344
Ryan Williams <ryan.rpwilliams@gmail.com>
2024-01-03 16:39:37 -0500

    stashing progress

--- crates/wassy/src/instrument/monitor_inst.rs
+++ crates/wassy/src/instrument/monitor_inst.rs
@@ -7,85 +7,46 @@ use wasabi_wasm::Instr;
 use wasabi_wasm::Instr::*;
 use wasabi_wasm::Label;
 use wasabi_wasm::LocalOp;
-use wasabi_wasm::StoreOp::*;
+use wasabi_wasm::StoreOp;
 use wasabi_wasm::Val;
 use wasabi_wasm::ValType;
 use wasabi_wasm::ValType::*;
 use wasabi_wasm::{MemoryOp, Module};
 
 pub fn monitor_test(module: &mut Module) {
-    for (func_idx, func) in module.clone().functions() {
-        // println!("func_idx: {:?}", func_idx);
-        // println!("func: {:?}", func);
+    let print_func_idx = add_print_function(module);
+
+    for (func_idx, func) in module.functions_mut() {
+        let mut new_instrs = Vec::new();
 
         if let Some(func_code) = func.code() {
-            for (instr_idx, instr) in func_code.body.clone().into_iter().enumerate() {
-                // println!("instr_idx: {:?}", instr_idx);
-                // println!("instr: {:?}", instr);
-
+            for (_, instr) in func_code.body.clone().into_iter().enumerate() {
                 if let Store(store_op, _) = instr {
-                    let func_type = store_op.to_type();
-                    let stack_size = func_type.inputs().len();
-
-                    let store_addr_type = func_type.inputs()[0];
-                    let store_val_type = func_type.inputs()[1];
-
-                    // Debugging stuff.
-                    // println!("store_addr_type: {:?}", store_addr_type);
-                    // println!("store_val_type: {:?}", store_val_type);
-                    // println!("store_op: {:?}", store_op);
-                    // println!("stack_size: {:?}", stack_size);
-                    // println!("func_type: {:?}", func_type);
-                    // println!("instr: {:?}", instr);
-
-                    // Insert a 'tee_local' instruction to duplicate the value being stored.
-                    let tee_instr = Local(LocalOp::Tee, Idx::from(2u32));
-                    // func_code.body.insert(instr_idx, tee_instr);
-
-                    // Insert a call to a host function that prints the value being stored.
-                    let print_func_idx = add_print_function(module);
-                    let call_instr = Instr::Call(print_func_idx);
-                    // func_code.body.insert(instr_idx + 1, call_instr);
-
-                    if store_addr_type != I32 {
-                        println!("[Instruction Monitor] Encountered a 'store' instruction where the address is of type '{store_addr_type}', skipping !");
-                        continue;
-                    }
-
                     match store_op {
-                        I32Store8 | I32Store16 | I32Store => {
-                            if store_val_type != I32 {
-                                println!("[Instruction Monitor] Encountered a 'i32.store' instruction where the value being stored is of type '{store_val_type}', skipping !");
-                                continue;
-                            }
+                        StoreOp::I32Store
+                        | StoreOp::I64Store
+                        | StoreOp::F32Store
+                        | StoreOp::F64Store => {
+                            new_instrs.push(Call(print_func_idx)); // Print the value on stack.
+                            new_instrs.push(Local(LocalOp::Get, Idx::from(0u32)));
+                            // Continue normal execution.
                         }
-                        I64Store8 | I64Store16 | I64Store32 | I64Store => {
-                            if store_val_type != I64 {
-                                println!("[Instruction Monitor] Encountered a 'i64.store' instruction where the value being stored is of type '{store_val_type}', skipping !");
-                                continue;
-                            }
-                        }
-                        F32Store => {
-                            if store_val_type != F32 {
-                                println!("[Instruction Monitor] Encountered a 'f32.store' instruction where the value being stored is of type '{store_val_type}', skipping !");
-                                continue;
-                            }
-                        }
-                        F64Store => {
-                            if store_val_type != F64 {
-                                println!("[Instruction Monitor] Encountered a 'f64.store' instruction where the value being stored is of type '{store_val_type}', skipping !");
-                                continue;
-                            }
-                        }
-                    };
+                        _ => {}
+                    }
                 }
+
+                // Add original instructions to the new instruction list.
+                new_instrs.push(instr.clone());
             }
         }
+
+        // Replace the original instructions with the new instruction list.
+        // *func.body_mut() = new_instrs;
     }
 }
 
-
 fn add_print_function(module: &mut Module) -> Idx<Function> {
+    // println!("[Instruction Monitor] Adding a print function to the module.");
     // Define the type of the print function.
     let print_func_type = FunctionType::new(&vec![ValType::I32], &[]);
 
