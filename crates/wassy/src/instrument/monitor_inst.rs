abe05f6d49eac274be6e33fb3b10c5fed37a2123
Ryan Williams <ryan.rpwilliams@gmail.com>
2024-01-03 17:35:11 -0500

    temp stash

--- crates/wassy/src/instrument/monitor_inst.rs
+++ crates/wassy/src/instrument/monitor_inst.rs
@@ -14,53 +14,53 @@ use wasabi_wasm::ValType::*;
 use wasabi_wasm::{MemoryOp, Module};
 
 pub fn monitor_test(module: &mut Module) {
-    let print_func_idx = add_print_function(module);
+    let logging_func_idx = add_logging_function(module);
 
     for (func_idx, func) in module.functions_mut() {
-        let mut new_instrs = Vec::new();
+        if let Some(instrs) = func.instrs_mut() {
+            let mut new_instrs = Vec::new();
 
-        if let Some(func_code) = func.code() {
-            for (_, instr) in func_code.body.clone().into_iter().enumerate() {
+            for instr in instrs.iter().cloned() {
                 if let Store(store_op, _) = instr {
                     match store_op {
                         StoreOp::I32Store
                         | StoreOp::I64Store
                         | StoreOp::F32Store
                         | StoreOp::F64Store => {
-                            new_instrs.push(Call(print_func_idx)); // Print the value on stack.
+                            new_instrs.push(Call(logging_func_idx)); // Print the value on stack.
                             new_instrs.push(Local(LocalOp::Get, Idx::from(0u32)));
                             // Continue normal execution.
                         }
                         _ => {}
                     }
                 }
-
                 // Add original instructions to the new instruction list.
-                new_instrs.push(instr.clone());
+                new_instrs.push(instr);
             }
+            // Replace the original instructions with the new ones.
+            *instrs = new_instrs;
         }
-
-        // Replace the original instructions with the new instruction list.
-        // *func.body_mut() = new_instrs;
     }
 }
 
-fn add_print_function(module: &mut Module) -> Idx<Function> {
+fn add_logging_function(module: &mut Module) -> Idx<Function> {
     // println!("[Instruction Monitor] Adding a print function to the module.");
     // Define the type of the print function.
-    let print_func_type = FunctionType::new(&vec![ValType::I32], &[]);
+    let log_func_type = FunctionType::new(&vec![ValType::I32, ValType::I32, ValType::I32], &[]);
 
-    // Define the body of the print function
+    // Define the body of the logging function
     // This is a placeholder, replace it with the actual instructions
-    let print_func_body = vec![
-        Local(LocalOp::Get, Idx::from(0u32)),
+    let log_func_body = vec![
+        Local(LocalOp::Get, Idx::from(0u32)), // Get the function index
+        Local(LocalOp::Get, Idx::from(1u32)), // Get the instruction index
+        Local(LocalOp::Get, Idx::from(2u32)), // Get the value to be stored
         Call(Idx::from(0u32)), // Call the host function
         End,
     ];
 
     // Add the function to the module
-    let print_func_idx = module.add_function(print_func_type, vec![I32], print_func_body);
+    let log_func_idx = module.add_function(log_func_type, vec![I32, I32, I32], log_func_body);
 
-    // Return the index of the print function.
-    print_func_idx
+    // Return the index of the logging function.
+    log_func_idx
 }
